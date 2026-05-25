ebe8c16a6bf1a211353198bd72d3fa81260ad0d5
luisRP2510 <luisreyyes25@gmail.com>
2026-05-24 21:49:19 -0600

    Add files via upload

---
+++ GASTOS.R
@@ -0,0 +1,21 @@
+#Gastos totales
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+celular <- 300
+transporte <- 240 
+comestibles <- 1527
+gimnasio <- 400
+alquiler <- 1500
+otros <- 1833
+
+total <- celular + transporte + comestibles + gimnasio + alquiler + otros
+
+total * 5
+
+total + 10
+
+gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+
+barplot(gastos)
+
+sort(gastos)
